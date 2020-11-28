0c2ecdc7e5c2122f897e59383ee68c3d87560912
Gionata Massi <gionata.massi@savoiabenincasa.it>
2020-11-28 12:52:48 +0100

    Rimozione della dichiarazione del namespace

--- 20201128/basi_linguaggio/main.cpp
+++ 20201128/basi_linguaggio/main.cpp
@@ -4,12 +4,15 @@
  * @author Gionata Massi <gionata.massi@savoiabenincasa.it>
  * @data 20201128
  */
+
+// copia il contenuto del file iostream
+// e di tutti i file da esso inclusi
 #include <iostream>
 
-using namespace std;
+// using namespace std;
 
 int main()
 {
-    cout << "Ciao mondo!" << endl;
+    std::cout << "Ciao mondo!" << std::endl;
     return 0;
 }
