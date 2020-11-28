cd39a9c2f48319fe3310727b72da24aee041aaaf
Gionata Massi <gionata.massi@savoiabenincasa.it>
2020-11-28 12:32:30 +0100

    Illustrazione delle basi del linguaggio

---
+++ 20201128/basi_linguaggio/main.cpp
@@ -0,0 +1,15 @@
+/* Il programma stampa una scritta a video
+ *
+ * @file main.cpp
+ * @author Gionata Massi <gionata.massi@savoiabenincasa.it>
+ * @data 20201128
+ */
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Ciao mondo!" << endl;
+    return 0;
+}
